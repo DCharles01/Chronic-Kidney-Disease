745932af2dc33eec3a5ec11c1136b6897f9808fb
DCharles01 <dcharles@kippnyccp.org>
2023-08-19 22:08:02 -0400

    Change runs-on OS for Github Actions and flask host
    Changed runs-on OS from macOS to ubuntu and set flask host to 0.0.0.0 to list on all available network for ec2 instance

--- utils/setup_ec2_instance_env.sh
+++ utils/setup_ec2_instance_env.sh
@@ -1,4 +1,24 @@
+# check list of available python versions: sudo yum install python3*
 # sudo yum install python36
+
+# sudo yum install -y centos-release-scl
+# sudo yum-config-manager --enable rhel-server-rhscl-7-rpms
+# sudo yum install -y python310
+#  sudo yum install sqlite
+
+# sudo yum update -y
+# sudo yum install docker -y
+# sudo systemctl status docker
+# sudo systemctl start docker
+# if above command doesn't work do this to add current ssh user to docker group
+# sudo usermod -aG docker $USER
+# start docker automatically
+# sudo systemctl enable docker
+
+# install docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+
 
 # sudo yum install git
 
@@ -8,6 +28,7 @@
 
 # sudo apt-get install python-pip
 
+# install pip
 # curl -O https://bootstrap.pypa.io/get-pip.py # download pip
 
 # python3 get-pip.py --user

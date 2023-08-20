5b25acd1647cc7ed79253326229e7cec064f37bf
DCharles01 <dcharles@kippnyccp.org>
2023-08-19 21:29:15 -0400

    Added github actions workflow
    Added deploy.yml file for github actions and new pem file for ec2 instance

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import datetime
 import os
 import logging
 import requests
+import json
 # from utils.model_prediction import predict
 
 
@@ -23,7 +24,7 @@ logging.basicConfig(format=log_format, datefmt='%m/%d/%Y %I:%M:%S %p', handlers=
 # Post-MVP: allow user to upload csv or excel with multiple patient data and return file with prediction
 
 # generate random session id
-session_id = secrets.token_hex(16)
+session_id = secrets.token_hex(16) # streamlit refreshes and this will change everytime an input is changed
 session_start_ts_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # timestamp
 session_start_ts = datetime.datetime.now()
 session_dt = datetime.date.today().strftime("%Y-%m-%d") # today's date
@@ -31,11 +32,14 @@ session_dt = datetime.date.today().strftime("%Y-%m-%d") # today's date
 
 logging.info('Creating Database and Tables')
 # run python script to create sqlite db and crete table
-os.system('python setup_sqlite_db_schema.py')
+# if os.path.isfile('ml_streamlit_application_data.db'):
+    # connect to database
+conn = sqlite3.connect('ml_streamlit_application_data.db')
+# else:
+#     os.system('python setup_sqlite_db_schema.py')
 logging.info('Done. Database and Table created.')
 
-# connect to database
-conn = sqlite3.connect('ml_streamlit_application_data.db')
+
 
 # create cursor
 cursor = conn.cursor()
@@ -47,8 +51,28 @@ INSERT INTO ckd_patient_data (session_id, session_ts, sesstion_dt, session_durat
 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 '''
 
+# initialize variables to access outside of following if statement
+prediction_start_ts_str = None
+prediction_start_ts = None
+prediction_end = None
+prediction = None
+prediction_dt = None
+prediction_duration = None
+
 # Define the streamlit app
 def app():
+
+    # define global variable to make accessible outside of if statement
+    global prediction_start_ts_str
+    global prediction_start_ts
+    global prediction_end
+    global prediction
+    global prediction_dt
+    global prediction_duration
+    global prediction_start_ts_str
+    global prediction_start_ts
+
+
     # Set the title of the app
     st.title(f'Patient CKD Prediction\nSession ID: {session_id}')
     # Ask the user for the inputs
@@ -77,7 +101,7 @@ def app():
     diabetes_prediction_mapping = {'yes':1, 'no':0}
     anemia_prediction_mapping = {'yes':1, 'no':0}
     pedal_edema_prediction_mapping = {'yes':1, 'no':0}
-    prediction_mapping = {'ckd': 'Patient Has CKD', 'notckd':'Patient Does Not Have CKD'}
+    
 
     # feature engineer columns
     red_blood_cells = rbc_prediction_mapping[red_blood_cells]
@@ -92,45 +116,43 @@ def app():
     # convert to json format for model api
     cols = ['age','blood_pressure','red_blood_cell_count','white_blood_cell_count','packed_cell_volume','serum_creatinine','sodium','potassium','hemoglobin','red_blood_cells','coronary_artery_disease','appetite','hypertension','diabetes','anemia','pedal_edema']
     vals = [age,blood_pressure,red_blood_cell_count,white_blood_cell_count,packed_cell_volume,serum_creatinine,sodium,potassium,hemoglobin,red_blood_cells,coronary_artery_disease,appetite,hypertension,diabetes,anemia,pedal_edema]
-    data = dict(zip(cols,vals))
+    data_vals = dict(zip(cols,vals))
     
 
-    # initialize variables to access outside of following if statement
-    # prediction_start_ts_str = None
-    # prediction_start_ts = None
-    # prediction_end = None
-    # prediction = None
-    # prediction_dt = None
-    # prediction_duration = None
-
     
-
+    
+    logging.info(data_vals)
     # # Make a prediction based on the inputs
     if st.button('Predict'):
         # define global variable to make accessible outside of if statement
-        global prediction_start_ts_str
-        global prediction_start_ts
-        global prediction_end
-        global prediction
-        global prediction_dt
-        global prediction_duration
-        global prediction_start_ts_str
-        global prediction_start_ts
+        # global prediction_start_ts_str
+        # global prediction_start_ts
+        # global prediction_end
+        # global prediction
+        # global prediction_dt
+        # global prediction_duration
+        # global prediction_start_ts_str
+        # global prediction_start_ts
 
         prediction_start_ts_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # prediction timestamp
         prediction_start_ts = datetime.datetime.now() # prediction timestamp
 
-        try:
-            logging.debug('Making Prediction')
+        # try:
+        logging.info('Making Prediction')
+        # create json object from data dictionary
+        data_json = json.dumps(data_vals)
+        logging.info(data_json)
 
-            # Make a POST request to the Flask API
-            response = requests.post('http://localhost:5000/predict', json=data)
-            prediction = response.json()['prediction']
-            #prediction = predict(model, blood_pressure, red_blood_cell_count, white_blood_cell_count, packed_cell_volume, serum_creatinine, sodium, potassium, hemoglobin, age, red_blood_cells, coronary_artery_disease, appetite, hypertension, diabetes, anemia, pedal_edema)
-            logging.info(f'Prediction Successful: {prediction}')
-        except:
-            logging.critical('Prediction Not Made.')
-            st.experimental_rerun() # refresh page if prediction is not returned
+        # headers
+        headers = {'Content-Type': 'application/json'}
+        # Make a POST request to the Flask API
+        response = requests.post('localhost:5000/predict', data=data_json, headers=headers)
+        prediction = response.text
+        #prediction = predict(model, blood_pressure, red_blood_cell_count, white_blood_cell_count, packed_cell_volume, serum_creatinine, sodium, potassium, hemoglobin, age, red_blood_cells, coronary_artery_disease, appetite, hypertension, diabetes, anemia, pedal_edema)
+        logging.info(f'Prediction Successful: {prediction}')
+        # except:
+        #     logging.critical('Prediction Not Made.')
+        #     st.experimental_rerun() # refresh page if prediction is not returned
         
         prediction_end = datetime.datetime.now() # prediction timestamp
         # prediction duration to track if prediction is taking a long time
@@ -147,17 +169,22 @@ def app():
     is_correct = st.radio('Is this prediction correct?', ('Yes', 'No'))
 
     # not_correct = st.checkbox('This prediction is not correct!')
+    logging.info(f'True Prediction Duration:{prediction_duration}')
+    logging.info(f'True Prediction:{prediction}')
 
-    # add submit button for user to officially send predictions to database    
-    if is_correct == 'Yes':
-        if st.button('Submit'):
+    # add submit button for user to officially send predictions to database
+    if st.button('Submit'):
+        
+        if is_correct == 'Yes':
+        
 
             session_end = datetime.datetime.now()
     
             # calculate session duration
             session_duration = (session_end - session_start_ts).total_seconds()
-
-             # values to insert into sqlite database
+            logging.info(f'Prediction Duration:{prediction_duration}')
+            logging.info(f'Prediction:{prediction}')
+            # values to insert into sqlite database
             values_to_insert = (session_id, session_start_ts_str, session_dt, session_duration, prediction_start_ts_str, prediction_dt, age, blood_pressure, 
                                 red_blood_cell_count, white_blood_cell_count, packed_cell_volume, serum_creatinine, sodium, potassium, hemoglobin, red_blood_cells,
                                 coronary_artery_disease, appetite, hypertension, diabetes, anemia, pedal_edema, prediction, prediction_duration, is_correct)
@@ -172,15 +199,16 @@ def app():
             st.write('Thank you for confirming Prediction! We will use this to improve the model')
             logging.debug('Session Finished. Predictions inserted into database')
     
-    else:
-        if st.button('Submit'):
-
+        else:
+                
+            logging.info(f'Prediction Duration:{prediction_duration}')
+            logging.info(f'Prediction:{prediction}')
             session_end = datetime.datetime.now()
     
             # calculate session duration
             session_duration = (session_end - session_start_ts).total_seconds()
 
-             # values to insert into sqlite database
+            # values to insert into sqlite database
             values_to_insert = (session_id, session_start_ts_str, session_dt, session_duration, prediction_start_ts_str, prediction_dt, age, blood_pressure, 
                                 red_blood_cell_count, white_blood_cell_count, packed_cell_volume, serum_creatinine, sodium, potassium, hemoglobin, red_blood_cells,
                                 coronary_artery_disease, appetite, hypertension, diabetes, anemia, pedal_edema, prediction, prediction_duration, is_correct)

5a253e431934bd747f0dd57c9bee7dfb4a441fc7
DCharles01 <dcharles@kippnyccp.org>
2023-08-19 21:29:15 -0400

    Added github actions workflow
    Added deploy.yml file for github actions and new pem file for ec2 instance

--- decision_tree_api_v1.py
+++ decision_tree_api_v1.py
@@ -1,18 +1,30 @@
 from flask import Flask, request, jsonify
-import joblib
+import pickle
+import json
 
 app = Flask(__name__)
 
 # Load the machine learning model
 
-model = joblib.load('model_api/decision_tree_model_0429.pkl')
+model = pickle.load(open('model_api/model_decisiontree_0801.pkl', 'rb'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json  # Assuming the input data is sent as JSON
+    data = request.get_json()
+    
     user_inputs = [data['age'], data['blood_pressure'], data['red_blood_cell_count'], data['white_blood_cell_count'], 
-                   data['packed_cell_volume'], data['serum_creatinine'], data['sodium'], data['potassium'], data['hemoglobin'], 
-                   data['red_blood_cells'], data['coronary_artery_disease'], data['appetite'], data['hypertension'], data['diabetes'], 
-                   data['anemia'], data['pedal_edema']]
-    prediction = model.predict([user_inputs])
-    return jsonify({'prediction': prediction[0]})
+                data['packed_cell_volume'], data['serum_creatinine'], data['sodium'], data['potassium'], data['hemoglobin'], 
+                data['red_blood_cells'], data['coronary_artery_disease'], data['appetite'], data['hypertension'], data['diabetes'], 
+                data['anemia'], data['pedal_edema']]
+    
+    prediction = model.predict([user_inputs])[0]
+    prediction_mapping = {0: 'Patient Has CKD', 1:'Patient Does Not Have CKD'}
+    return prediction_mapping[prediction]
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+

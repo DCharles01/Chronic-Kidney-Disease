3c668624e2e0afb1b28694ed1b548dc69d1a703c
DCharles01 <dcharles@kippnyccp.org>
2024-01-18 00:53:25 -0500

    file additions, model creation
    add init file
    add .gitattributes file
    initialized react project to build form
    moved models/ to model_api/ to use SQLAlchemy to interact with form data, use orm for data cleaning and feed cleaned data to decision tree API endpoint

---
+++ backend/model_api/models/models.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class CKDPatientData(db.Model):
+    __tablename__ = 'ckd_patient_data'
+
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.String(255))
+    session_ts = db.Column(db.String(255))
+    sesstion_dt = db.Column(db.String(255))
+    session_duration = db.Column(db.Integer)
+    prediction_ts = db.Column(db.String(255))
+    prediction_dt = db.Column(db.String(10))  # Assuming 'yyyy-mm-dd' format
+    age = db.Column(db.Integer)
+    blood_pressure = db.Column(db.Integer)
+    red_blood_cell_count = db.Column(db.Integer)
+    white_blood_cell_count = db.Column(db.Integer)
+    packed_cell_volume = db.Column(db.Integer)
+    serum_creatinine = db.Column(db.Integer)
+    sodium = db.Column(db.Integer)
+    potassium = db.Column(db.Integer)
+    hemoglobin = db.Column(db.Integer)
+    red_blood_cells = db.Column(db.String(255))
+    coronary_artery_disease = db.Column(db.String(255))
+    appetite = db.Column(db.String(255))
+    hypertension = db.Column(db.String(255))
+    diabetes = db.Column(db.String(255))
+    anemia = db.Column(db.String(255))
+    pedal_edema = db.Column(db.String(255))
+    prediction = db.Column(db.String(255))
+    prediction_duration = db.Column(db.Integer)
+    is_correct = db.Column(db.String(255))

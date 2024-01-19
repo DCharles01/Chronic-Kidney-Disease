9d112f7f4de9677cf3ecdde255f02579fc806549
DCharles01 <dcharles@kippnyccp.org>
2024-01-19 01:37:34 -0500

    add postgres db functions and tests

--- backend/db/postgres_connector.py
+++ backend/db/postgres_connector.py
@@ -1,12 +1,19 @@
-from dotenv import load_dotenv
-import os
+from .settings import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-load_dotenv('backend/.env')
+database_url: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+dev_database_url: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DEV_DB_NAME}"
+test_database_url: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{TEST_DB_NAME}"
 
-DB_USER = os.environ.get('USERNAME')
-DB_PASSWORD = os.environ.get('PASSWORD')
-DB_NAME = os.environ.get('DB')
-DEV_DB_NAME = os.environ.get('DEV_DB')
-TEST_DB_NAME = os.environ.get('TEST_DB')
-DB_HOST = os.environ.get('HOST')
-DB_PORT = os.environ.get('PORT')
+
+def create_session(db_url: str):
+    # breakpoint()
+    engine = create_engine(db_url)  # create database engine
+
+    Session = sessionmaker(bind=engine)  # create session factory
+
+    session = Session()
+
+    return session
+

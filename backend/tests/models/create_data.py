050f80e31be53f2b174f029a50957d831b664650
DCharles01 <dcharles@kippnyccp.org>
2024-01-20 04:15:23 -0500

    added create_app tests for flask

---
+++ backend/tests/models/create_data.py
@@ -0,0 +1,11 @@
+from model_api.models.models import CKDPatientData
+from db.postgres_connector import create_session, database_url, dev_database_url, test_database_url
+import pytest
+
+
+@pytest.fixture()
+def insert_data():
+    patient_data = CKDPatientData(session_id='123', age=25, hypertension='yes', diabetes='yes', pedal_edema='yes', prediction='CKD')
+
+
+

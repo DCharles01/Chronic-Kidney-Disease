765b7d569fc7120bdb8ca5a8f6261705ebb5a585
DCharles01 <dcharles@kippnyccp.org>
2024-01-20 05:01:35 -0500

    added more tests to test_postgres_tables

--- backend/tests/db/test_postgres_tables.py
+++ backend/tests/db/test_postgres_tables.py
@@ -1,22 +1,61 @@
 import pytest
 from model_api.create_app import create_app, create_table
-from models_api.models.models import db
+from model_api.models.models import db
+
 
 @pytest.fixture()
-def app():
-    app = create_app('dev')
+def dev_app():
+    app = create_app(environ='dev')
     with app.app_context():
         # build up
-        create_table()
+        create_table(app=app, environ='dev')
+        yield app
+
+        # tear down
+        db.drop_all()
+
+@pytest.fixture()
+def prod_app():
+    app = create_app(environ='prod')
+    with app.app_context():
+        # build up
+        create_table(app=app, environ='prod')
+        yield app
+
+        # tear down
+        db.drop_all()
+
+@pytest.fixture()
+def test_app():
+    app = create_app(environ='test')
+    with app.app_context():
+        # build up
+        create_table(app=app, environ='test')
         yield app
 
         # tear down
         db.drop_all()
 
 
-def test_table_exists_in_dev_env(app):
+def test_table_exists_in_dev_db(dev_app):
     inspector = db.inspect(db.engine)
 
     table_names = inspector.get_table_names()
 
     assert 'patient_data' in table_names
+
+def test_table_exists_in_prod_db(prod_app):
+    inspector = db.inspect(db.engine)
+
+    table_names = inspector.get_table_names()
+
+    assert 'patient_data' in table_names
+
+def test_table_exists_in_test_db(test_app):
+    inspector = db.inspect(db.engine)
+
+    table_names = inspector.get_table_names()
+
+    assert 'patient_data' in table_names
+
+
